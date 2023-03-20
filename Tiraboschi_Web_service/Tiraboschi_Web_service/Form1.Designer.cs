174df3d0624c7cc07ca5c1f624719f8a8d018bfd
oltira <fabio.tira04@gmail.com>
2023-03-20 13:31:08 +0100

    progettoWeb_service

---
+++ Tiraboschi_Web_service/Tiraboschi_Web_service/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace TEP_WebService
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(97, 211);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(97, 82);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(112, 289);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(65, 47);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "invia";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(230, 100);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Tiraboschi_Web_service/Tiraboschi_Web_service/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace TEP_WebService
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            TEP_WebService.init();
+        }
+        public class Informazioni_Film
+        {
+            public string Title { get; set; }
+            public string Year { get; set; }
+            public string Runtime { get; set; }
+            public string Genre { get; set; }
+            public string Plot { get; set; }
+            public string Director { get; set; }
+            public string Writer { get; set; }
+            public string Actors { get; set; }
+            public string Language { get; set; }
+            public string Country { get; set; }
+            public string Awards { get; set; }
+            public string imdbRating { get; set; }
+            public string BoxOffice { get; set; }
+            public string Poster { get; set; }
+        }
+        public class TEP_WebService
+        {
+            static HttpClient client = new HttpClient();
+            public static void init()
+            {
+                client.BaseAddress = new Uri("https://www.omdbapi.com");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+            }
+            public static async Task<Informazioni_Film> OttieniInformazioni(string path)
+            {
+                Informazioni_Film richiesta = null;
+                HttpResponseMessage response = await client.GetAsync(path);
+                if (response.IsSuccessStatusCode)
+                {
+                    richiesta = await JsonSerializer.DeserializeAsync<Informazioni_Film>(await response.Content.ReadAsStreamAsync());
+                }
+                return richiesta;
+            }
+        }
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            string titolo = textBox1.Text.Replace(" ", "+");
+            string informazioni = "/?apikey=4e2bf5e3=" + titolo;
+            Informazioni_Film a = new Informazioni_Film();
+            a = await TEP_WebService.OttieniInformazioni(informazioni);
+            if (a != null)
+            {
+                richTextBox1.Text = "TITOLO: " + a.Title + "\n"
+                + "USCITA: " + a.Year + "\n"
+                + "GENERE: " + a.Genre + "\n"
+                + "TRAMA: " + a.Plot + "\n"
+                + "DURATA: " + a.Runtime + "\n"
+                + "REGISTA: " + a.Director + "\n"
+                + "SCENEGGIATORI: " + a.Writer + "\n"
+                + "CAST: " + a.Actors + "\n"
+                + "LINGUA: " + a.Language + "\n"
+                + "PAESE DI PRODUZIONE: " + a.Country + "\n"
+                + "PREMI: " + a.Awards + "\n"
+                + "VOTAZIONE FILM: " + a.imdbRating + "\n"
+                + "INCASSI USA: " + a.BoxOffice + "\n";
+                pictureBox1.LoadAsync(a.Poster);
+            }
+            else
+                MessageBox.Show("Errore");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
